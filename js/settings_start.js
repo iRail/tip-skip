fce63af609f6c539812160402c2673c241aa33f4
Sjimi <roose.dimitri@gmail.com>
2012-07-09 11:07:46 +0200

    Updating settings.html

--- js/settings_start.js
+++ js/settings_start.js
@@ -1,8 +1,8 @@
 $('document').ready(function() {
     $.each(ticketCategories, function(key, value) {
         $('<div>').attr({
-            id : 'div-' + key,
+            id : 'categorie-' + key,
             class : 'span3'
-        }).appendTo('#div-row-fluid').append('<h2>' + value.name + '</h2><p>' + value.settings + '</p>');
+        }).appendTo('div#row-fluid-1').append('<h2>' + value.name + '</h2><p>' + '</p>');
     });
 });
