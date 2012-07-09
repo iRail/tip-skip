2c684e164c88ffe8d53083dfe0abcdb3878472fc
Sjimi <roose.dimitri@gmail.com>
2012-07-09 11:06:50 +0200

    Updating index.html, adding current tickets and buy option

--- js/index_start.js
+++ js/index_start.js
@@ -1,8 +1,16 @@
 $('document').ready(function() {
     $.each(ticketCategories, function(key, value) {
         $('<div>').attr({
-            id : 'div-' + key,
-            class : 'span3'
-        }).appendTo('#div-row-fluid').append('<h2>' + value.name + '</h2><p>' + value.text + '</p>');
+            id : 'categorie-' + key,
+            class : 'span3',
+        }).appendTo('div#row-fluid-1').html('<h2>' + value.name + '<a class="btn btn-primary pull-right" href="' + value.buyPage + '">Buy</a></h2><p>' + value.text + '</p>');
+
+        /*Modernizr nog invoegen*/
+        var tickets = localStorage['tickets'];
+        if (tickets != null) {
+            $.each(tickets, function(index, value) {
+                /*<ul class="nav nav-list"><li><a href="#">Ticket 1</a></li><li><a href="#">Ticket 2</a></li></ul>'*/
+            })
+        }
     });
 });
