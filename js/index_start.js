1f3ca944e67f0977ecf6e239913cf8dc68aa03c1
Sjimi <roose.dimitri@gmail.com>
2012-07-10 09:49:34 +0200

    Adding De Lijn ticket buying functionality

--- js/index_start.js
+++ js/index_start.js
@@ -1,16 +1,40 @@
 $('document').ready(function() {
-    $.each(ticketCategories, function(key, value) {
+    $.each(ticketCategories, function(categorieID, categorie) {
         $('<div>').attr({
-            id : 'categorie-' + key,
-            class : 'span3',
-        }).appendTo('div#row-fluid-1').html('<h2>' + value.name + '<a class="btn btn-primary pull-right" href="' + value.buyPage + '">Buy</a></h2><p>' + value.text + '</p>');
+            class : 'span4',
+            id : categorieID
+        }).appendTo('div#row-fluid-1').html('<h2>' + categorie.name + '<a class="btn btn-primary pull-right" href="' + categorie.buy.buyPage + '">Buy</a></h2><p>' + categorie.text + '</p>');
 
-        /*Modernizr nog invoegen*/
-        var tickets = localStorage['tickets'];
-        if (tickets != null) {
-            $.each(tickets, function(index, value) {
-                /*<ul class="nav nav-list"><li><a href="#">Ticket 1</a></li><li><a href="#">Ticket 2</a></li></ul>'*/
+        if (localStorage.getItem(categorieID) != null) {
+            $('<ul>').attr({
+                class : 'nav nav-list',
+                id : categorieID
+            }).appendTo('div#' + categorieID);
+            $.each($.parseJSON(localStorage[categorieID]), function(ticketID, ticket) {
+                if (ticket.active) {
+                    $('<li>').attr({
+                        class : 'ticket',
+                        id : ticketID
+                    }).appendTo('ul#' + categorieID);
+
+                    $('<a>').appendTo('li#' + ticketID);
+                }
             })
         }
     });
+
+    setInterval(function() {
+        $.each($('li.ticket'), function(key, value) {
+            var tickets = $.parseJSON(localStorage.getItem(value.parentNode.id));
+            var ticket = tickets[value.id];
+            var endTime = new Date().getTime();
+            var elapsedTime = Math.round((ticket.general.validTime - (endTime - ticket.startTime) / 1000) / 60);
+            $(this).find('a:first').html(ticket.general.text + '<p align="right">(' + elapsedTime + ' minutes remaining)</p>');
+            if (elapsedTime <= 0) {
+                ticket.active = false;
+                tickets[value.id] = ticket;
+                localStorage.setItem(value.parentNode.id, JSON.stringify(tickets));
+            }
+        });
+    }, 1000);
 });
--- js/tip-skip.js
+++ js/tip-skip.js
@@ -1,42 +1,36 @@
 var ticketCategories = {
-    1 : {
+    dl : {
         name : 'De Lijn',
-        page : '',
+        buy : {
+            buyPage : 'buy_dl.html',
+            categories : {
+                oneHourTicket : {
+                    text : 'De Lijn - 60',
+                    validTime : 3600,                },
+                twoHourTicket : {
+                    text: 'De Lijn - 120',
+                    validTime : 7200
+                }
+            }
+        },
         text : 'De Lijn makes it easy for you: you can pay for your journey with your mobile phone. You do this by purchasing an SMS ticket: a valid ticket for your journey with the bus or tram throughout Flanders.',
-        settings : '<p><h3>Tijdsduur</h3>60 minuten<br>120 minuten</p>'
+        settings : ''
     },
-    2 : {
+    4411 : {
         name : '4411',
-        page : '',
+        buy : {
+            buyPage : 'buy_4411.html'
+        },
         text : 'To make it even easier to use SMS parking, Mobile-for has developed a few applications for smartphone users.',
-        settings : '<p><h3>Nummerplaat</h3>ABC-123</p>'
+        settings : ''
     },
-    3 : {
+    nmbs : {
         name : 'NMBS',
-        page : '',
+        buy : {
+            buyPage : 'buy_nmbs.html'
+        },
         text : 'We maken het je graag makkelijk. Daarom kun je voortaan je treinbiljet overal en altijd aankopen! Eenvoudig, koop je treinbiljet met je mobiele telefoon en reis met je SMS.',
-        settings : '<p><h3>Vertraging</h3>5 minuten<br>10 minuten<br>20 minuten<br>AFGESCHAFT</p>'
+        settings : ''
     },
-    /* Test purposes 
-    5 : {
-        name : 'De Lijn',
-        page : '',
-        text : 'De Lijn makes it easy for you: you can pay for your journey with your mobile phone. You do this by purchasing an SMS ticket: a valid ticket for your journey with the bus or tram throughout Flanders.'
-    },
-    6 : {
-        name : '4411',
-        page : '',
-        text : 'To make it even easier to use SMS parking, Mobile-for has developed a few applications for smartphone users.'
-    },
-    7 : {
-        name : 'NMBS',
-        page : '',
-        text : 'We maken het je graag makkelijk. Daarom kun je voortaan je treinbiljet overal en altijd aankopen! Eenvoudig, koop je treinbiljet met je mobiele telefoon en reis met je SMS.'
-    },
-    8 : {
-        name : 'Taxi Ticket',
-        page : '',
-        text : 'Taxi ticket is een zelf uitgevonden SMS ticket categorie. Deze zal worden gebruikt bij het testen van de applicatie'
-    },*/ 
 };
 
